0df711e169abd25942337fab8416f2e10b23d278
[dyad] <cardapioweb7@gmail.com>
2025-11-24 13:14:44 -0300

    [dyad] wrote 1 file(s)

--- src/components/cardapio/Banner.tsx
+++ src/components/cardapio/Banner.tsx
@@ -9,26 +9,9 @@ export function Banner({ logoUrl, borderColor }: BannerProps) {
       position: 'relative', 
       height: '180px', 
       overflow: 'hidden',
-      backgroundColor: '#FF0000' // Vermelho
+      backgroundColor: 'transparent' // Transparente - sem cor
     }}>
-      {/* Forma curva e inclinada usando SVG */}
-      <svg 
-        style={{ 
-          position: 'absolute', 
-          top: 0, 
-          left: 0, 
-          width: '100%', 
-          height: '100%'
-        }} 
-        viewBox="0 0 448 180" 
-        preserveAspectRatio="none"
-      >
-        {/* Caminho curvo e inclinado */}
-        <path 
-          d="M 0,0 L 448,0 L 448,120 Q 400,140 350,145 Q 300,150 250,145 Q 200,140 150,135 Q 100,130 50,125 Q 25,122 0,120 Z" 
-          fill="#FF0000" // Vermelho
-        />
-      </svg>
+      {/* Removido o SVG e qualquer elemento visual */}
     </div>
   )
 }
