09b13ce247ced55496b94348fd32aeaf419028f1
[dyad] <cardapioweb7@gmail.com>
2025-12-05 20:51:23 -0300

    [dyad] Removed the "Cadastrar Primeiro Produto" button from EmptyState component - wrote 1 file(s) + extra files edited outside of Dyad

--- src/components/admin/EmptyState.tsx
+++ src/components/admin/EmptyState.tsx
@@ -18,17 +18,6 @@ export function EmptyState({ selectedCategory, onNewProduct }: EmptyStateProps) 
           : 'Tente selecionar outra categoria ou cadastre novos produtos'
         }
       </p>
-      {selectedCategory === 'todas' && (
-        <div className="flex justify-center">
-          <Button 
-            onClick={onNewProduct}
-            className="bg-gray-600 hover:bg-gray-700 text-white font-semibold px-8 py-3 rounded-xl shadow-lg hover:shadow-xl transition-all duration-200"
-          >
-            <Plus className="w-5 h-5 mr-2" />
-            Cadastrar Primeiro Produto
-          </Button>
-        </div>
-      )}
     </div>
   )
 }
