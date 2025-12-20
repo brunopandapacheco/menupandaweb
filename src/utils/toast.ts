c8a4d04c24f0a906831731465e1235285280149e
[dyad] <cardapioweb7@gmail.com>
2025-12-20 14:47:32 -0300

    [dyad] wrote 1 file(s) + extra files edited outside of Dyad

--- src/utils/toast.ts
+++ src/utils/toast.ts
@@ -1,6 +1,6 @@
 import { toast } from 'sonner'
 
-export const showSuccess = (message: string) => toast.success(message, { duration: 2000 })
-export const showError = (message: string) => toast.error(message, { duration: 3000 })
+export const showSuccess = (message: string) => toast.success(message, { duration: 1000 })
+export const showError = (message: string) => toast.error(message, { duration: 1500 })
 export const showLoading = (message: string) => toast.loading(message)
 export const dismissToast = (toastId: string) => toast.dismiss(toastId)
